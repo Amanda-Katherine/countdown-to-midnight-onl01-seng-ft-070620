e4e14dfae2393f0dfde620eec1d541591a63453a
Amanda Walker <walker.akw@gmail.com>
2020-07-07 20:01:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -15,8 +15,8 @@ countdown(12)
 def countdown_with_sleep(num)
   until num == 0
     puts "#{number} SECOND(S)!"
-    number -= 1
     sleep 1.seconds
+    number -= 1
   end
   "HAPPY NEW YEAR!"
 end
